92bc22aea4b5a1ed365d61f3161576c66921b782
itsLeonB <ellionblessan@gmail.com>
2025-06-08 15:30:27 +0700

    feat: add transaction utils

---
+++ internal/transactor.go
@@ -0,0 +1,74 @@
+package internal
+
+import (
+	"context"
+	"log"
+
+	"github.com/itsLeonB/ezutil/config"
+	"github.com/rotisserie/eris"
+	"gorm.io/gorm"
+)
+
+type GormTransactor struct {
+	DB *gorm.DB
+}
+
+func (t *GormTransactor) Begin(ctx context.Context) (context.Context, error) {
+	tx := t.DB.WithContext(ctx).Begin()
+	if err := tx.Error; err != nil {
+		return nil, eris.Wrap(err, config.MsgTransactionError)
+	}
+
+	return context.WithValue(ctx, config.ContextKeyGormTx, tx), nil
+}
+
+func (t *GormTransactor) Commit(ctx context.Context) error {
+	tx, err := GetTxFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	if tx != nil {
+		err = tx.WithContext(ctx).Commit().Error
+		if err != nil {
+			return eris.Wrap(err, config.MsgTransactionError)
+		}
+	}
+
+	return nil
+}
+
+func (r *GormTransactor) Rollback(ctx context.Context) {
+	tx, err := GetTxFromContext(ctx)
+	if err != nil {
+		log.Println("rollback error:", err)
+		return
+	}
+	if tx == nil {
+		log.Println("no transaction is running")
+		return
+	}
+
+	err = tx.WithContext(ctx).Rollback().Error
+	if err != nil {
+		if err.Error() == "sql: transaction has already been committed or rolled back" {
+			return
+		}
+
+		log.Printf("error: %T", err)
+		log.Println("rollback error:", err)
+	}
+}
+
+func GetTxFromContext(ctx context.Context) (*gorm.DB, error) {
+	trx := ctx.Value(config.ContextKeyGormTx)
+	if trx != nil {
+		tx, ok := trx.(*gorm.DB)
+		if !ok {
+			return nil, eris.New("error getting tx from ctx")
+		}
+
+		return tx, nil
+	}
+
+	return nil, nil
+}
--- main.go
+++ main.go
@@ -96,22 +96,32 @@ func PreloadRelations(relations []string) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
-func WithinTransaction(db *gorm.DB, fn func(tx *gorm.DB) error) error {
-	tx := db.Begin()
-	if tx.Error != nil {
-		return eris.Wrap(tx.Error, "failed to begin transaction")
+type Transactor interface {
+	Begin(ctx context.Context) (context.Context, error)
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context)
+}
+
+func NewTransactor(db *gorm.DB) Transactor {
+	return &internal.GormTransactor{DB: db}
+}
+
+func GetTxFromContext(ctx context.Context) (*gorm.DB, error) {
+	return internal.GetTxFromContext(ctx)
+}
+
+func WithinTransaction(ctx context.Context, transactor Transactor, serviceFn func(ctx context.Context) error) error {
+	ctx, err := transactor.Begin(ctx)
+	if err != nil {
+		return eris.Wrap(err, "error starting transaction")
+	}
+	defer transactor.Rollback(ctx)
+
+	if err := serviceFn(ctx); err != nil {
+		return eris.Wrap(err, "error executing service function")
 	}
 
-	if err := fn(tx); err != nil {
-		tx.Rollback()
-		return fmt.Errorf("transaction failed: %w", err)
-	}
-
-	if err := tx.Commit().Error; err != nil {
-		return fmt.Errorf("failed to commit transaction: %w", err)
-	}
-
-	return nil
+	return transactor.Commit(ctx)
 }
 
 // endregion
