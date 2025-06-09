c129f7f90318799bfc3972befd03db02478433e8
itsLeonB <ellionblessan@gmail.com>
2025-06-09 18:19:56 +0700

    feat: add route setup

--- main.go
+++ main.go
@@ -196,6 +196,50 @@ func GetFromContext[T any](ctx *gin.Context, key string) (T, error) {
 	return asserted, nil
 }
 
+type RouteConfig struct {
+	Group    string
+	Versions []RouteVersionConfig
+	Handlers []gin.HandlerFunc
+}
+
+type RouteVersionConfig struct {
+	Version  int
+	Groups   []RouteGroupConfig
+	Handlers []gin.HandlerFunc
+}
+
+type RouteGroupConfig struct {
+	Group     string
+	Endpoints []EndpointConfig
+	Handlers  []gin.HandlerFunc
+}
+
+type EndpointConfig struct {
+	Method   string
+	Endpoint string
+	Handlers []gin.HandlerFunc
+}
+
+func SetupRoutes(router *gin.Engine, routeConfigs []RouteConfig) {
+	if router == nil {
+		log.Fatal("Router cannot be nil")
+	}
+
+	for _, routeConfig := range routeConfigs {
+		routeGroup := router.Group(routeConfig.Group, routeConfig.Handlers...)
+		for _, versionConfig := range routeConfig.Versions {
+			versionGroup := routeGroup.Group(fmt.Sprintf("/v%d", versionConfig.Version, versionConfig.Handlers))
+			for _, routeGroupConfig := range versionConfig.Groups {
+				group := versionGroup.Group(routeGroupConfig.Group, routeGroupConfig.Handlers...)
+				for _, endpointConfig := range routeGroupConfig.Endpoints {
+					group.Handle(endpointConfig.Method, endpointConfig.Endpoint, endpointConfig.Handlers...)
+				}
+			}
+
+		}
+	}
+}
+
 // endregion
 
 // region Gorm Utils
